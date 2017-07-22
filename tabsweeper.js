aa6a383fd4a35b57251e8324bcb27a2e9c42778c
Christopher De Cairos <cade@mozillafoundation.org>
2017-07-21 21:21:36 -0400

    refactor the background script

--- tabsweeper.js
+++ tabsweeper.js
@@ -1,99 +1,80 @@
 const notifications = browser.notifications;
 const tabs = browser.tabs;
 const storage = browser.storage;
-const disallowedURLs = [
-    "chrome:",
-    "javascript:",
-    "data:",
-    "file:",
-    "moz-extension:",
-    "about:"
-];
 
-let tabWweeperNotification = "tabWweeper save success";
+class TabSweeper {
+    constructor() {
+        this.urls = {
+            iconUrl: "icons/tabsweeper.png",
+            sidebarUrl: "/sidebar/panel.html"
+        };
 
-/*
- * Display a message
- */
-function notify(message, title) {
-    return notifications.create(tabWweeperNotification, {
-        type: "basic",
-        message,
-        title,
-        iconUrl: browser.extension.getURL("icons/tabsweeper.png")
-    }).then(id => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                notifications.clear(id).then(resolve, reject);
-            });
+        this.messages = {
+            pressToOpen: "\n\nPress Ctrl+Shift+Y to toggle the Tab Sweeper sidebar",
+            nothingToClean: "Your browser tabs are already clean!",
+            tabsSaved: "Tabs saved and closed!"
+        };
+
+        this.disallowedURLs = [
+            "chrome:",
+            "javascript:",
+            "data:",
+            "file:",
+            "moz-extension:",
+            "about:"
+        ];
+
+        this.sessions = [];
+
+        browser.browserAction.onClicked.addListener(() => this.cleanup());
+        browser.commands.onCommand.addListener((command) => this.onCommand(command));
+    }
+
+    /*
+     * Clean up open tabs and save data to local storage
+     */
+    cleanup() {
+        this.getTabs()
+        .then(result => this.filterTabs(result))
+        .then(result => this.loadSavedSessions(result))
+        .then(result => this.saveSession(result))
+        .catch(e => console.error(e));
+    }
+
+    /*
+     * get open tabs in the window that aren't pinned
+     */
+    getTabs() {
+        return tabs.query({
+            currentWindow: true,
+            pinned: false
+        }).then(openTabs => {
+            return { openTabs };
         });
-    });
-}
+    }
 
-/*
- * save sessions to local storage
- */
-function saveSessions(sessions) {
-    return storage.local.set({sessions});
-}
-
-/*
- * close a set of tabs
- */
-function closeTabs(tabIDs) {
-    return tabs.remove(tabIDs);
-}
-
-/*
- * Create a blank tab, so the window stays open
- */
-function createBlankTab() {
-    return tabs.create({});
-}
-
-/*
- * get sessions from local storage
- */
-function getSavedSessions() {
-    return storage.local.get();
-}
-
-/*
- * get open tabs in the window that aren't pinned
- */
-function getTabs() {
-    return tabs.query({
-        currentWindow: true,
-        pinned: false
-    });
-}
-
-/*
- * get open tabs
- */
-function cleanup() {
-    let tabs,
-        tabIDs,
-        allTabsClosing = false,
-        nothingToClean = false;
-
-    getTabs()
-    .then(openTabs => {
-
+    /*
+     * Filter the open tabs list for unsupported URI schemes
+     * extract and store information about tab count and URLs
+     */
+    filterTabs(result) {
+        let openTabs = result.openTabs;
         let tabCount = openTabs.length;
 
         // filter out disallowed URLs
         openTabs = openTabs.filter(tab =>
-            !disallowedURLs.some(scheme => tab.url.startsWith(scheme))
+            !this.disallowedURLs.some(scheme => tab.url.startsWith(scheme))
         );
-
         if (openTabs.length === 0) {
-            return nothingToClean = true;
+            result.nothingToClean = true;
+            return result;
         }
 
-        tabIDs = openTabs.map(tab => tab.id);
+        result.nothingToClean = false;
 
-        tabs = openTabs.map(tab => {
+        result.tabIDs = openTabs.map(tab => tab.id);
+
+        result.tabsToSave = openTabs.map(tab => {
             return {
                 url: tab.url,
                 title: tab.title,
@@ -101,44 +82,94 @@ function cleanup() {
             }
         });
 
-        if (tabCount === tabIDs.length) {
-            allTabsClosing = true;
-        }
+        result.allTabsClosing = tabCount === result.tabIDs.length;
 
-        return getSavedSessions();
-    })
-    .then(result => {
-        if (nothingToClean) {
-            return notify("Your browser tabs are already clean!", "Success");
-        }
+        return result;//loadSavedSessions();
+    }
 
-        if (!result || !result.sessions) {
-            result.sessions = [];
-        }
-
-        let created = Date.now();
-
-        result.sessions.unshift({ created, tabs });
-
-        return saveSessions(result.sessions)
-        .then(() => {
-            if (allTabsClosing) {
-                return createBlankTab()
+    /*
+     * get sessions from local storage
+     */
+    loadSavedSessions(result) {
+        return storage.local.get()
+        .then(storageData => {
+            if (storageData && storageData.sessions) {
+                this.sessions = storageData.sessions;
             }
-            return Promise.resolve();
-        })
-        .then(() => closeTabs(tabIDs))
-        .then(() => notify("Tabs saved and closed!", "Success"));
-    })
-    .catch(e => console.error(e));
+            return result;
+        });
+    }
+
+    /*
+     * Save the session to local storage
+     */
+    saveSession(result) {
+        console.log(result.nothingToClean)
+        if (result.nothingToClean) {
+            return this.notify(`${this.messages.nothingToClean}${this.messages.pressToOpen}`);
+        }
+
+        this.sessions.unshift({
+            created: Date.now(),
+            tabs: result.tabsToSave
+        });
+
+        return this.saveSessionsToStorage(result)
+        .then((result) => this.createBlankTab(result))
+        .then((result) => this.closeTabs(result))
+        .then(() => this.notify(`${this.messages.tabsSaved}${this.messages.pressToOpen}`));
+    }
+
+    /*
+     * save sessions to local storage
+     */
+    saveSessionsToStorage(result) {
+        return storage.local.set({ sessions: this.sessions })
+        .then(() => result);
+    }
+
+    /*
+     * Create a blank tab, so the window stays open
+     */
+    createBlankTab(result) {
+        if (result.allTabsClosing) {
+           return tabs.create({})
+           .then(() => result);
+        }
+
+        return result;
+    }
+
+    /*
+     * close a set of tabs
+     */
+    closeTabs(result) {
+        return tabs.remove(result.tabIDs).then(() => result);
+    }
+
+    /*
+     * Display a notification
+     */
+    notify(message) {
+        return notifications.create("TabSweeper-notify", {
+            message,
+            type: "basic",
+            title: "Complete",
+            iconUrl: browser.extension.getURL(this.urls.iconUrl)
+        }).then(id => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    notifications.clear(id).then(resolve, reject);
+                });
+            });
+        });
+    }
+
+    onCommand(command) {
+        if (command == "execute-cleanup") {
+            this.cleanup();
+        }
+    }
 }
 
-browser.browserAction.onClicked.addListener(cleanup);
-
-browser.commands.onCommand.addListener(function(command) {
-  if (command == "open-list-in-tab") {
-    tabs.create({
-        url: "/sidebar/panel.html"
-    });
-  }
-});
+new TabSweeper();
